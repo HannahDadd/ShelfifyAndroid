0f1f5f621dada2d0966ee12b0264626ddad40cfb
Hannah Dadd <hannahbdadd@googlemail.com>
2026-06-16 08:59:14 +0100

    background

---
+++ app/src/main/java/hannah/bd/shelfify/views/homepage/BackgroundView.kt
@@ -0,0 +1,50 @@
+package hannah.bd.shelfify.views.homepage
+
+import android.R.attr.height
+import android.R.attr.x
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fitInside
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.contentType
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntOffset
+import hannah.bd.shelfify.R
+import kotlin.random.Random
+
+@Composable
+fun backGroundView() {
+    val index = (0..3).random()
+    
+    when (index) {
+        0 -> {
+            rainView()
+        }
+        1 -> {
+            Image(
+                painter = painterResource(id = R.drawable.day),
+                contentDescription = "day",
+                contentScale = ContentScale.Fit
+            )
+        }
+        2 -> {
+            Image(
+                painter = painterResource(id = R.drawable.dawn),
+                contentDescription = "day",
+                contentScale = ContentScale.Fit
+            )
+        }
+        3 -> {
+            Image(
+                painter = painterResource(id = R.drawable.night),
+                contentDescription = "day",
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
+}
