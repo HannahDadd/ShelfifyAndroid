a43c5f41a59553618f4578979a580c456e40e57b
Hannah Dadd <hannahbdadd@googlemail.com>
2026-07-12 22:14:23 +0100

    graphs for writer

--- app/src/main/java/hannah/bd/shelfify/views/graphs/GraphForWriter.kt
+++ app/src/main/java/hannah/bd/shelfify/views/graphs/GraphForWriter.kt
@@ -1,12 +1,10 @@
 package hannah.bd.shelfify.views.graphs
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -15,7 +13,6 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,31 +24,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.aay.compose.barChart.BarChart
-import com.aay.compose.barChart.model.BarParameters
-import com.aay.compose.baseComponents.model.GridOrientation
-import com.aay.compose.lineChart.LineChart
-import com.aay.compose.lineChart.model.LineParameters
-import com.aay.compose.lineChart.model.LineType
 import com.patrykandpatryk.vico.compose.axis.horizontal.bottomAxis
 import com.patrykandpatryk.vico.compose.axis.vertical.startAxis
 import com.patrykandpatryk.vico.compose.chart.Chart
 import com.patrykandpatryk.vico.compose.chart.column.columnChart
+import com.patrykandpatryk.vico.compose.chart.line.lineChart
 import com.patrykandpatryk.vico.core.entry.FloatEntry
 import com.patrykandpatryk.vico.core.entry.entryModelOf
-import com.patrykandpatryk.vico.view.chart.line.lineChart
 import hannah.bd.shelfify.R
 import hannah.bd.shelfify.modals.AppDatabase
 import hannah.bd.shelfify.modals.Stat
 import hannah.bd.shelfify.modals.UserPreferences
+import java.time.LocalDate
+import java.util.Calendar
+import java.util.Date
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GraphForWriter(
@@ -76,7 +67,7 @@ fun GraphForWriter(
             CenterAlignedTopAppBar(
                 title = {
                     Text(
-                        "️✏️ Congrats on your writing progress ✏️",
+                        "See your writing progress",
                     )
                 },
                 navigationIcon = {
@@ -92,51 +83,97 @@ fun GraphForWriter(
     ) { innerPadding ->
         Column(modifier = Modifier.fillMaxSize()) {
             LazyColumn(
-                modifier = Modifier.fillMaxSize()
-                    .padding(innerPadding),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(16.dp),
                 verticalArrangement = Arrangement.spacedBy(15.dp),
                 horizontalAlignment = Alignment.Start,
             ) {
                 item {
-                    Column(modifier = Modifier.padding(16.dp)) {
-                        Text("Words written in the app: ${ wordsWritten }",
-                            fontFamily = FontFamily(Font(R.font.bellefairregularfont)))
-                        Text("Library completes at 100k words.",
-                            fontFamily = FontFamily(Font(R.font.bellefairregularfont)),)
-                    }
+                    Text("Words written in the app: ${ wordsWritten }",
+                        fontFamily = FontFamily(Font(R.font.bellefairregularfont)))
+                    Text(
+                        "Library completes at 100k words.",
+                        fontFamily = FontFamily(Font(R.font.bellefairregularfont)),
+                    )
                 }
                 item {
-                    if (stats.size < 2) {
-                        Column(modifier = Modifier.padding(16.dp)) {
-                            Text("Not enough statistics to show yet! Head over to grow your writing and get those words written.",
-                                fontFamily = FontFamily(Font(R.font.bellefairregularfont)))
-                        }
-                    } else {
-                        val totalWords = stats.sumOf { it.wordsWritten }
-                        Column(modifier = Modifier.padding(16.dp)) {
-                            Text(
-                                "You've partaken in ${stats.size} writing sprints.",
-                                fontFamily = FontFamily(Font(R.font.bellefairregularfont))
-                            )
+                    Text(
+                        "You've finished ${stats.size} writing sprints.",
+                        fontFamily = FontFamily(Font(R.font.bellefairregularfont))
+                    )
+                }
+                item {
+                    Column(verticalArrangement = Arrangement.spacedBy(15.dp)) {
 
-                            val entryModel = entryModelOf(stats.mapIndexed { index, stat ->
-                                FloatEntry(index.toFloat(), stat.wordsWritten.toFloat())
-                            })
-                            Chart(
-                                chart = columnChart(),
-                                model = entryModel,
-                                startAxis = startAxis(),
-                                bottomAxis = bottomAxis(),
-                            )
-                            Chart(
-                                chart = lineChart(),
-                                model = entryModel,
-                                startAxis = startAxis(),
-                                bottomAxis = bottomAxis(),
-                            )
-                        }
+                        val entryModel = entryModelOf(stats.mapIndexed { index, stat ->
+                            FloatEntry(index.toFloat(), stat.wordsWritten.toFloat())
+                        })
+                        Text("Words written across all your sprints",
+                            fontFamily = FontFamily(Font(R.font.bellefairregularfont)))
+                        Chart(
+                            chart = lineChart(),
+                            model = entryModel,
+                            startAxis = startAxis(),
+                            bottomAxis = bottomAxis(),
+                        )
                     }
                 }
+//                item {
+//                    Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
+//                        val cal: Calendar = Calendar.getInstance()
+//                        cal.add(Calendar.MONTH, -1)
+//                        val result: Date = cal.getTime()
+//
+//                        val statsLastMonth = stats
+//                            .filter { it.date.after(cal.getTime()) }
+//
+//                        val bestStat = statsLastMonth.maxBy { it.wordsWritten }
+//
+//                        val entryModel = entryModelOf(statsLastMonth
+//                            .mapIndexed { index, stat ->
+//                            FloatEntry(index.toFloat(), stat.wordsWritten.toFloat())
+//                        })
+//                        Text("Sprints finished in the last month: ${ statsLastMonth.size }",
+//                            fontFamily = FontFamily(Font(R.font.bellefairregularfont)))
+//                        Chart(
+//                            chart = columnChart(),
+//                            model = entryModel,
+//                            startAxis = startAxis(),
+//                            bottomAxis = bottomAxis(),
+//                        )
+//                        Text("Your best sprint in the last month was on ${ bestStat.date.toString() }! You wrote ${bestStat.wordsWritten}. Congrats!",
+//                            fontFamily = FontFamily(Font(R.font.bellefairregularfont)))
+//                    }
+//                }
+//                item {
+//                    Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
+//                        val cal: Calendar = Calendar.getInstance()
+//                        cal.add(Calendar.YEAR, -1)
+//                        val result: Date = cal.getTime()
+//
+//                        val statsLastYear = stats
+//                            .filter { it.date.after(cal.getTime()) }
+//
+//                        val bestStat = statsLastYear.maxBy { it.wordsWritten }
+//
+//                        val entryModel = entryModelOf(statsLastYear
+//                            .mapIndexed { index, stat ->
+//                                FloatEntry(index.toFloat(), stat.wordsWritten.toFloat())
+//                            })
+//                        Text("Sprints finished in the last month: ${ statsLastYear.size }",
+//                            fontFamily = FontFamily(Font(R.font.bellefairregularfont)))
+//                        Chart(
+//                            chart = lineChart(),
+//                            model = entryModel,
+//                            startAxis = startAxis(),
+//                            bottomAxis = bottomAxis(),
+//                        )
+//                        Text("Your best sprint in the last year was on ${ bestStat.date.toString() }! You wrote ${bestStat.wordsWritten}.",
+//                            fontFamily = FontFamily(Font(R.font.bellefairregularfont)))
+//                    }
+//                }
             }
         }
     }
